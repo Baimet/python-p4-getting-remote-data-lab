b6badb8be713a188d83607939b436011d50a1907
baimet <isaacbaimetyatich@gmail.com>
2024-10-08 12:48:27 +0300

    Get remote data

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,9 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content # Returns the body of the response as bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body) # Converts the response text to Python list/dict
